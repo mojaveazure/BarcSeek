882bfa91ccdcc664d6fb7611d775cee52aea65cb
Paul Hoffman <phoffman@nygenome.org>
2018-02-15 00:54:15 -0500

    Add extra information to setup.py

--- setup.py
+++ setup.py
@@ -7,18 +7,25 @@ if not (sys.version_info.major == 3 and sys.version_info.minor >= 5):
     sys.exit("Please use Python 3.5 or higher")
 
 
+import warnings
 from setuptools import setup
 from setuptools.command.install import install
 
+warnings.filterwarnings('ignore')
+
 # Some basic information
-NAME = 'BarcSeek'
-URL = 'https://github.com/mojaveazure/BarcSeek'
+NAME = 'BarcSeek' # type: str
+DESCRIPTION = 'A general barcode partitioning tool for demultiplexing genomic sequencing data'
+VERSION = '0.1.0' # type: str
+AUTHORS = '\n'.join(("Paul Hoffman", "Joseph Kubiak", "Brian Joseph", "James Abraham", "Tobias Schraink")) # type: str
+URL = 'https://github.com/mojaveazure/BarcSeek' # type: str
+LICENSE = 'MIT' # type: str
 
 #   Platforms
-PLATFORMS = ['Linux', 'Mac OS-X', 'UNIX']
+PLATFORMS = ['Linux', 'Mac OS-X', 'UNIX'] # type: List[str]
 
 #   Classifiers
-CLASSIFIERS = [
+CLASSIFIERS = [ # type: List[str]
     #   See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     #   For more classfiiers
     #   How mature is this project? Common values are
@@ -45,36 +52,40 @@ CLASSIFIERS = [
 ]
 
 #   Specify Python version
-PYTHON_REQUIRES = '>=3.5'
+PYTHON_REQUIRES = '>=3.5' # type: str
 
 #   Dependencies
-INSTALL_REQUIRES = [
+INSTALL_REQUIRES = [ # type: List[str]
     'regex',
     'biopython'
 ]
 
 #   Packages
 PACKAGE_DIR = 'barcseek'
-PACKAGES = [
+PACKAGES = [ # type: List[str]
     PACKAGE_DIR
 ]
 
 #   Entry points into the program
-ENTRY_POINTS = {
+ENTRY_POINTS = { # type: Dict[str, List[str]]
     'console_scripts': [
         '%(name)s = %(package)s.%(package)s:main' % {'name': NAME, 'package': PACKAGE_DIR}
     ]
 }
 
 #   Commands available for setup.py
-CMD_CLASS = {
+CMD_CLASS = { # type: Dict[str, type]
     'install': install
 }
 
 #   Run setup
 setup(
     name=NAME.lower(),
+    description=DESCRIPTION,
+    version=VERSION,
+    author=AUTHORS,
     url=URL,
+    license=LICENSE,
     platforms=PLATFORMS,
     python_requires=PYTHON_REQUIRES,
     classifiers=CLASSIFIERS,
