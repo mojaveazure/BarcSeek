bb1c486f08cc526ea3cdb47f8da47c9cd04c5895
Paul Hoffman <phoffman@nygenome.org>
2018-02-14 22:23:44 -0500

    Update arguments

--- barcseek/arguments.py
+++ barcseek/arguments.py
@@ -8,6 +8,36 @@ if not (sys.version_info.major == 3 and sys.version_info.minor >= 5):
 
 
 import argparse
+import textwrap
+import multiprocessing
+
+_HELP_WRAP = 60 # type: int
+_ERROR_DEFAULT = 1 # type: int
+_VERBOSITY_DEFAULT = 'info' # type: str
+_VERBOSITY_LEVELS = (
+    'debug',
+    'info',
+    'warning',
+    'error',
+    'critical'
+)
+
+def _help_text(text: str, wrap: int=45) -> str:
+    return '\n'.join(textwrap.wrap(text, wrap))
+
+def _num_cores(value: str) -> int:
+    try:
+        value = int(value) # type: int
+    except ValueError:
+        raise argparse.ArgumentTypeError("Must pass an integer value")
+    try:
+        assert value < multiprocessing.cpu_count()
+    except AssertionError:
+        raise argparse.ArgumentTypeError("Cannot have more jobs (%s) than cores available (%s)" % (value, multiprocessing.cpu_count()))
+    except NotImplementedError:
+        pass
+    return value
+
 
 def set_args() -> argparse.ArgumentParser:
     """Make an argument parser"""
@@ -25,61 +55,93 @@ def set_args() -> argparse.ArgumentParser:
         formatter_class=argparse.RawTextHelpFormatter,
         add_help=True
     )
-    parser.add_argument( # Forward FASTQ
+    #   Arguments for verbosity and logging
+    parser.add_argument( # Verbosity
+        '-v',
+        '--verbosity',
+        dest='verbosity',
+        type=str.lower,
+        choices=_VERBOSITY_LEVELS,
+        default=_VERBOSITY_DEFAULT,
+        required=False,
+        metavar='verbosity',
+        help=_help_text(text="Set the verbosity level, choose from '%s'; defaults to '%s'" % ("', '".join(_VERBOSITY_LEVELS), _VERBOSITY_DEFAULT))
+    )
+    parser.add_argument( # Log file
+        '-l',
+        '--logfile',
+        dest='logfile',
+        type=str,
+        default=None,
+        required=False,
+        metavar='log file',
+        # help=_help_text(text="Specify a file for the log messages, defaults to stderr")
+        help=argparse.SUPPRESS
+    )
+    #   Input arguments
+    inputs = parser.add_argument_group(
+        title='input arguments',
+        description='Provide inputs for %(prog)s'
+    )
+    inputs.add_argument( # Forward FASTQ
         '-f',
         '--forward-fastq',
         dest='forward',
         type=str,
         default=None,
         metavar='FORWARD FASTQ',
-        help="Provide a filepath for the Forward FASTQ file.\n[REQUIRED]",
+        help=_help_text(text="Provide a filepath for the forward FASTQ file"),
         required=True
     )
-    parser.add_argument( # Reverse FASTQ
+    inputs.add_argument( # Reverse FASTQ
         '-r',
         '--reverse-fastq',
         dest='reverse',
         type=str,
         default=None,
         metavar='REVERSE FASTQ',
-        help="Provide a filepath for the Reverse FASTQ file.\n[OPTIONAL]"
+        help=_help_text(text="Provide a filepath for the reverse FASTQ file")
     )
-    parser.add_argument( # Sample sheet
+    inputs.add_argument( # Sample sheet
         '-s',
         '--sample-sheet',
         dest='sample',
         type=str,
         default=None,
         metavar='SAMPLE SHEET',
-        help="Provide a filepath for the Sample Sheet file.\n[REQUIRED]",
+        help=_help_text(text="Provide a filepath for the sample sheet"),
         required=True
     )
-    parser.add_argument( # Barcodes file
+    inputs.add_argument( # Barcodes file
         '-b',
         '--barcodes',
         dest='barcodes',
         type=str,
         default=None,
         metavar='BARCODES',
-        help="Provide a filepath for the Barcodes CSV file.\n[REQUIRED]",
+        help=_help_text(text="Provide a filepath for the barcodes CSV file"),
         required=True
     )
-    parser.add_argument( # Number of errors allowed
+    barcodes = parser.add_argument_group(
+        title='barcode options',
+        description="Set parameters for barcode demultiplexing"
+    )
+    barcodes.add_argument( # Number of errors allowed
         '-e',
         '--error',
         dest='error',
         type=int,
-        default=1,
+        default=_ERROR_DEFAULT,
         metavar='ERROR',
-        help="This is how many mismatches in the barcode \nwe allowed before rejecting.\n[OPTIONAL, DEFAULT=1]"
+        help=_help_text(text="This is how many mismatches in the barcode we allowed before rejecting, defaults to %s" % _ERROR_DEFAULT)
     )
-    parser.add_argument( # Numlines?
-        '-l',
-        '--numlines',
-        dest='numlines',
-        type=int,
-        default=40000,
-        metavar='NUMLINES',
-        help='We internally split your input file(s) into \nmany smaller files, after -l lines.\n[OPTIONAL, DEFAULT=40000]'
-    )
+    # parser.add_argument( # Numlines?
+    #     '-l',
+    #     '--numlines',
+    #     dest='numlines',
+    #     type=int,
+    #     default=40000,
+    #     metavar='NUMLINES',
+    #     help='We internally split your input file(s) into \nmany smaller files, after -l lines.\n[OPTIONAL, DEFAULT=40000]'
+    # )
     return parser
